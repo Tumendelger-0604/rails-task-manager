6642113028617576dbba29500aa10dfd5c3d55e4
Tumendelger Gankhuyag <tumee666@gmail.com>
2023-09-08 16:42:54 +0200

    rails

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,55 +1,43 @@
 class TasksController < ApplicationController
-end
-def index
-  @tasks = Task.all
-end
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
-def show
-  @task = Task.find(params[:id])
-end
-
-def new
-  @task = Task.new
-end
-
-def create
-  @task = Task.new(task_params)
-  if @task.save
-    redirect_to tasks_path
-  else
-    render 'new'
+  def index
+    @tasks = Task.all
   end
-end
 
-private
-
-def task_params
-  params.require(:task).permit(:title, :details, :completed)
-end
-
-def edit
-  @task = Task.find(params[:id])
-end
-
-def update
-  @task = Task.find(params[:id])
-  if @task.update(task_params)
-    redirect_to tasks_path
-  else
-    render 'edit'
+  def show
   end
-end
 
-def destroy
-  @task = Task.find(params[:id])
-  @task.destroy
-  redirect_to tasks_path
-end
+  def new
+    @task = Task.new
+  end
 
-before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
+  end
 
-private
+  def edit
+  end
 
-def set_task
-  @task = Task.find(params[:id])
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
